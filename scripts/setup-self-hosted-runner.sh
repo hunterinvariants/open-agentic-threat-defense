48a03b40213969a9b601ed2e5b04e4acccbeca08
hunterinvariants <hunterinvariants@users.noreply.github.com>
2026-06-06 23:57:46 +0200

    Fix runner service install cwd

--- scripts/setup-self-hosted-runner.sh
+++ scripts/setup-self-hosted-runner.sh
@@ -92,6 +92,6 @@ runner ALL=(root) NOPASSWD: /usr/bin/install, /bin/ln, /bin/rm, /bin/chown, /bin
 EOF
 chmod 0440 /etc/sudoers.d/oadtd-runner
 
-"$runner_dir/svc.sh" install "$runner_user"
-"$runner_dir/svc.sh" start
-"$runner_dir/svc.sh" status
+sudo -u "$runner_user" bash -lc "cd '$runner_dir' && ./svc.sh install"
+sudo bash -lc "cd '$runner_dir' && ./svc.sh start"
+sudo bash -lc "cd '$runner_dir' && ./svc.sh status"
